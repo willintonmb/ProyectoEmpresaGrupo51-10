42e6a37ff8be7ce68147597799db60e20860a9aa
JulianLopez93 <andersson.1701522324@ucaldas.edu.co>
2021-10-11 11:49:55 -0500

    Metodo Eliminar Empleado

--- WebApp/Pages/Entities/Empleado/Eliminar.cshtml.cs
+++ WebApp/Pages/Entities/Empleado/Eliminar.cshtml.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Persistencia.Repositorios;
+using Persistencia.Interfaces;
+using Dominio;
 
 namespace MyApp.Namespace
 {
     public class EliminarEmpleadoModel : PageModel
     {
-        public void OnGet()
+private readonly IEmpleadoRepository _empleadoRepository;
+        public Empleado Empleado {get; set;}
+        public EliminarEmpleadoModel(IEmpleadoRepository empleadoRepository)
         {
+            _empleadoRepository = empleadoRepository;
+        }
+        public void OnGet(int empleadoId)
+        {
+            Empleado = _empleadoRepository.GetEmpleado(empleadoId);
+        }
+
+        public void OnPost(int empleadoId)
+        {
+            _empleadoRepository.DeleteEmpleado(empleadoId);
         }
     }
 }
