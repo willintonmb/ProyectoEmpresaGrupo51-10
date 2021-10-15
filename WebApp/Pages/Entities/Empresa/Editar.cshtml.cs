08ff21005dc9c85f8c0f1c38e6ad2eda16076bfd
Willinton Morales <willinton_morales@hotmail.com>
2021-10-14 21:51:36 -0500

    Crud empresa

--- WebApp/Pages/Entities/Empresa/Editar.cshtml.cs
+++ WebApp/Pages/Entities/Empresa/Editar.cshtml.cs
@@ -4,13 +4,54 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Dominio;
+using Persistencia.Repositorios;
+using Persistencia.Interfaces;
 
 namespace MyApp.Namespace
 {
     public class EditarEmpresaModel : PageModel
     {
-        public void OnGet()
+        private readonly IEmpresaRepository _empresaRepository;
+
+        [BindProperty]
+        public Empresa Empresa {get; set;}
+        public EditarEmpresaModel(IEmpresaRepository empresaRepository)
         {
+            _empresaRepository = empresaRepository;
+        }
+        public IActionResult OnGet(int empresaId)
+        {
+            if (empresaId != 0)
+            {
+                Empresa = _empresaRepository.GetEmpresa(empresaId);
+            }
+            else
+            {
+                Empresa = new Empresa();
+            }
+
+             if (Empresa == null)
+            {
+                return RedirectToPage("./Not Found");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (Empresa.Id > 0)
+            {
+                Empresa = _empresaRepository.UpdateEmpresa(Empresa);
+            }
+            else
+            {
+                _empresaRepository.AddEmpresa(Empresa);
+            }
+             return Page();
         }
     }
 }
--- WebApp/Pages/Entities/Empresa/Eliminar.cshtml.cs
+++ WebApp/Pages/Entities/Empresa/Eliminar.cshtml.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Persistencia.Repositorios;
+using Persistencia.Interfaces;
+using Dominio;
 
 namespace MyApp.Namespace
 {
     public class EliminarEmpresaModel : PageModel
     {
-        public void OnGet()
+        private readonly IEmpresaRepository _empresaRepository;
+        public Empresa Empresa {get; set;}
+        public EliminarEmpresaModel(IEmpresaRepository empresaRepository)
         {
+            _empresaRepository = empresaRepository;
+        }
+        public void OnGet(int empresaId)
+        {
+            Empresa = _empresaRepository.GetEmpresa(empresaId);
+        }
+
+        public void OnPost(int empresaId)
+        {
+            _empresaRepository.DeleteEmpresa(empresaId);
         }
     }
 }
