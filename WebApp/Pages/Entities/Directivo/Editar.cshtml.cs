b82bedf5cd3b392e1c140ee130275b3bdc218423
Willinton Morales <willinton_morales@hotmail.com>
2021-10-14 21:36:59 -0500

    Crud directivo

--- WebApp/Pages/Entities/Directivo/Editar.cshtml.cs
+++ WebApp/Pages/Entities/Directivo/Editar.cshtml.cs
@@ -4,13 +4,55 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Dominio;
+using Persistencia.Repositorios;
+using Persistencia.Interfaces;
 
 namespace MyApp.Namespace
 {
     public class EditarDirectivoModel : PageModel
     {
-        public void OnGet()
+        private readonly IDirectivoRepository _directivoRepository;
+
+        [BindProperty]
+        public Directivo Directivo {get; set;}
+        public EditarDirectivoModel(IDirectivoRepository directivoRepository)
         {
+            _directivoRepository = directivoRepository;
+        }
+        public IActionResult OnGet(int directivoId)
+        {
+
+            if (directivoId != 0)
+            {
+                Directivo = _directivoRepository.GetDirectivo(directivoId);
+            }
+            else
+            {
+                Directivo = new Directivo();
+            }
+ 
+            if (Directivo == null)
+            {
+                return RedirectToPage("./Not Found");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (Directivo.Id > 0)
+            {
+                Directivo = _directivoRepository.UpdateDirectivo(Directivo);
+            }
+            else
+            {
+                _directivoRepository.AddDirectivo(Directivo);
+            }
+             return Page();
         }
     }
 }
--- WebApp/Pages/Entities/Directivo/Eliminar.cshtml.cs
+++ WebApp/Pages/Entities/Directivo/Eliminar.cshtml.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Persistencia.Repositorios;
+using Persistencia.Interfaces;
+using Dominio;
 
 namespace MyApp.Namespace
 {
     public class EliminarDirectivoModel : PageModel
     {
-        public void OnGet()
+        private readonly IDirectivoRepository _directivoRepository;
+        public Directivo Directivo {get; set;}
+        public EliminarDirectivoModel(IDirectivoRepository directivoRepository)
         {
+            _directivoRepository = directivoRepository;
+        }
+        public void OnGet(int directivoId)
+        {
+            Directivo = _directivoRepository.GetDirectivo(directivoId);
+        }
+
+        public void OnPost(int directivoId)
+        {
+            _directivoRepository.DeleteDirectivo(directivoId);
         }
     }
 }
